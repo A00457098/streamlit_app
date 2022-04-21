ae8c0afe1d4f29bc3577c6f84c8b735c4ee570e7
Radhika Suri <Radhika.Suri@smu.ca>
2022-04-20 22:04:50 -0300

    Update bitcoin_prices_assignment.py

--- bitcoin_prices_assignment.py
+++ bitcoin_prices_assignment.py
@@ -22,29 +22,13 @@ def get_data(payload: dict):
         json_data = req.json()
         data=json_data['prices']
         df=pd.DataFrame(data,columns=['date','prices'])
+        df = pd.DataFrame(json_data['prices'], columns=['Date', currency])
     else:
         print(req.status_code)
     return df
 
-def plot_line_chart():
-    fig, ax = plt.subplots()
-    plt.grid()
-    ax.plot(df_date, df_prices)
-    ax.set_ylim(ymin=0)
-    ax.set_xlim(xmax=days)
-    # Axis multiplier
-    multiplier = 12
-
-    # Set a tick on each integer multiple
-    locator = plt.MultipleLocator(multiplier)
-
-    # Set the locator of the major ticker
-    ax.xaxis.set_major_locator(locator)
-
-    st.pyplot(fig)
 
 #Step 1: get data
-
 
 st.header("Bitcoin prices")
 days=st.slider("No of days",1,365,90)
@@ -58,36 +42,14 @@ if df is not None:
     #Step 2: Create dataframe
     #API returns prices and dates (in Unix epoch format). Use pandas to
     #convert the timestamp to human-readable dates
-
-    df['date']=pd.to_datetime(df['date'],unit='ms')
-    df_date=df['date'].dt.strftime("%d-%b")
-
-
-    df_prices=df['prices']
-
-    if currency=="usd":
-        df['prices'] = df['prices'].apply(lambda x: x/1.2613)
-    elif currency=="inr":
-        df['prices'] = df['prices'].apply(lambda x: x*60)
-
-
-    df['date']=df['date'].apply(lambda x:(x-datetime(2022,1,1)).days)
     
-    #Step 3: Plot linechart
-    plot_line_chart()
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    df = df.set_index('Date')
+    df_mean = df[currency].mean()
     
-    #Step4: Calculate mean price
+    #step3: Plot line chart
+    st.line_chart(df[currency])
 
-    df_mean = df['prices'].mean()
     str_to_display= f'Average price during this time was: {df_mean} in {currency}'
     st.text(str_to_display)
     
-
-
-
-
-
-
-
-
-
